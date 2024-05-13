0807aedc3344af3ab900c210621d38c7e2f387eb
paull-30 <lupu.paul30@yahoo.com>
2024-05-13 13:45:27 +0300

    completed auth route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -41,3 +41,46 @@ export const register = async (req, res) => {
     res.status(500).json({ message: 'Failed to create user!' });
   }
 };
+
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username }).exec();
+
+    if (!user) return res.status(400).json({ message: 'Invalid Credentials!' });
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid)
+      return res.status(400).json({ message: 'Invalid Credentials!' });
+
+    const age = 1000 * 60 * 60 * 24 * 7;
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: age }
+    );
+    const userInfo = user.toObject();
+    delete userInfo.password;
+
+    res
+      .cookie('token', token, {
+        httpOnly: true,
+        // secure:true,
+        maxAge: age,
+      })
+      .status(200)
+      .json(userInfo);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Failed to login!' });
+  }
+};
+
+export const logout = (req, res) => {
+  res.clearCookie('token').status(200).json({ message: 'Logout Successful' });
+};
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { register } from '../controllers/auth.controller.js';
+import { register, login, logout } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post('/register', register);
-// router.post('/login', login);
-// router.post('/logout', logout);
+router.post('/login', login);
+router.post('/logout', logout);
 
 export default router;

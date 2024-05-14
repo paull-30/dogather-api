755545242b4ad9f7c23324319a660f1bbb3120ac
paull-30 <lupu.paul30@yahoo.com>
2024-05-14 18:09:28 +0300

    finished user.controller

--- api/middleware/verifyToken.js
+++ api/middleware/verifyToken.js
@@ -7,7 +7,7 @@ export const verifyToken = (req, res, next) => {
 
   jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
     if (err) return res.status(403).json({ message: 'Token is not Valid!' });
-    req.userId = payload.id;
+    req.userId = String(payload.id);
 
     next();
   });

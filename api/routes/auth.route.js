42a099f62cf5b86cff6dc890390051433cdf5c77
Boualam3 <boualam.bourahla@gmail.com>
2024-07-13 11:31:57 +0100

    add check auth status endpoint , change client url in docker-compose to vite localhost

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,15 +1,17 @@
-import express from 'express';
-import { register, login, logout } from '../controllers/auth.controller.js';
-import { validateData } from '../middleware/validationMiddleware.js';
+import express from "express"
+import { register, login, logout } from "../controllers/auth.controller.js"
+import { validateData } from "../middleware/validationMiddleware.js"
 import {
   userLoginSchema,
   userRegistrationSchema,
-} from '../schemas/validationSchemas.js';
+} from "../schemas/validationSchemas.js"
+import { verifyToken } from "../middleware/verifyToken.js"
 
-const router = express.Router();
+const router = express.Router()
 
-router.post('/register', validateData(userRegistrationSchema), register);
-router.post('/login', validateData(userLoginSchema), login);
-router.post('/logout', logout);
+router.post("/register", validateData(userRegistrationSchema), register)
+router.post("/login", validateData(userLoginSchema), login)
+router.post("/authstatus", verifyToken, (req, res) => res.send())
+router.post("/logout", logout)
 
-export default router;
+export default router
